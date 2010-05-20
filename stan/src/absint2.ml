fe2d9acd9b8dc5f4146fa369fdfdb97c6332b408
Miron Brezuleanu <mbrezu@gmail.com>
2010-05-20 21:50:19 +0300

    Absint2: interpreter core, started implementing the 'uninitialized variable' pass.

--- stan/src/absint2.ml
+++ stan/src/absint2.ml
@@ -2,33 +2,53 @@
 open Utils;;
 open Printf;;
 
+type expr =
+  | Number of int
+  | Add of expr * expr
+  | Subtract of expr * expr
+  | Mod of expr * expr
+  | Divide of expr * expr
+  | Multiply of expr * expr
+  | Not of expr
+  | Var of string
+  | LessThan of expr * expr
+  | GreaterThan of expr * expr
+  | Equal of expr * expr;;
+
 type ir =
   | NewFrame
   | KillFrame
   | Goto of string
-  | GotoIf of string * string * string
+  | GotoIf of expr * string * string
   | Label of string
-  | Assignment of string * string
+  | Assignment of string * expr
   | Declare of string
   | Output of string;;
 
-type var_value =
-  | Uninitialized
-  | Unknown
+let example_1 = [ NewFrame;
+                  Declare "N";
+                  Assignment("N", Number 137);
+                  Label "While";
+                  GotoIf(Not(GreaterThan(Var "N", Number 1)), "AfterWhile", "");
+                  GotoIf(Equal(Mod(Var"N", Number 2), Number 0), "ElseBranch", "");
+                  Assignment("N", Divide(Var "N", Number 2));
+                  Goto("AfterIf");
+                  Label("ElseBranch");
+                  Assignment("N", Add(Multiply(Number 3, Var "N"), Number 1));
+                  Label("AfterIf");
+                  Goto("While");
+                  Label("AfterWhile");
+                  KillFrame;
+                ];;
 
-module VarMap = Map.Make(String);;
-
-type env = var_value VarMap.t list;;
+module StringMap = Map.Make(String);;
 
 type block = { name: string;
                labels: string list;
                instructions: ir list;
                next_blocks: string list;
                prev_blocks: string list;
-               end_state: env;
-               run_count: int};;
-
-module BlockMap = Map.Make(String);;
+             };;
 
 let gensym_counter = ref 0;;
 
@@ -81,9 +101,7 @@ let split_into_blocks ir_list =
         labels = labels;
         instructions = irs;
         next_blocks = [];
-        prev_blocks = [];
-        end_state = [];
-        run_count = 0}
+        prev_blocks = [] }
   in
   let add_block current_block blocks =
     if current_block = []
@@ -135,22 +153,6 @@ let process_goto_if ir_list =
   in
     pgi_iter ir_list;;
 
-let example_1 = [ NewFrame;
-                  Declare "N";
-                  Assignment("N", "137");
-                  Label "While";
-                  GotoIf("!(N > 1)", "AfterWhile", "");
-                  GotoIf("N mod 2 = 0", "ElseBranch", "");
-                  Assignment("N", "N/2");
-                  Goto("AfterIf");
-                  Label("ElseBranch");
-                  Assignment("N", "3 * N + 1");
-                  Label("AfterIf");
-                  Goto("While");
-                  Label("AfterWhile");
-                  KillFrame;
-                ];;
-
 let rebuild_ir blocks =
   blocks
              |> List.map (fun block -> block.instructions)
@@ -161,9 +163,9 @@ let map_of_list list =
     match list with
       | [] -> map
       | (key, value) :: tl ->
-          mol_iter (BlockMap.add key value map) tl
+          mol_iter (StringMap.add key value map) tl
   in
-    mol_iter BlockMap.empty list;;
+    mol_iter StringMap.empty list;;
 
 let make_block_maps blocks =
   let name_list = List.map (fun block -> (block.name, block)) blocks in
@@ -180,17 +182,17 @@ let add_prev_next block_map label_map =
     let last_insn = block.instructions |> List.rev |> List.hd in
       match last_insn with
         | Goto(lbl) ->
-            [block.name, (BlockMap.find lbl label_map).name]
+            [block.name, (StringMap.find lbl label_map).name]
         | GotoIf(_, lbl1, lbl2) ->
-            [block.name, (BlockMap.find lbl1 label_map).name;
-             block.name, (BlockMap.find lbl2 label_map).name]
+            [block.name, (StringMap.find lbl1 label_map).name;
+             block.name, (StringMap.find lbl2 label_map).name]
         | _ ->
             []
   in
   let from_to_links =
-    BlockMap.fold (fun block_name block from_to_links ->
-                     let new_links = next_blocks_links block in
-                       new_links @ from_to_links) block_map []
+    StringMap.fold (fun block_name block from_to_links ->
+                      let new_links = next_blocks_links block in
+                        new_links @ from_to_links) block_map []
   in
   let next_blocks block =
     List.filter (fun (fst, _) -> fst = block.name) from_to_links
@@ -200,65 +202,139 @@ let add_prev_next block_map label_map =
     List.filter (fun (_, snd) -> snd = block.name) from_to_links
              |> List.map (fun (prev, _) -> prev)
   in
-    BlockMap.map (fun block -> { block with
-                                   next_blocks = next_blocks block;
-                                   prev_blocks = prev_blocks block }) block_map;;
+    StringMap.map (fun block -> { block with
+                                    next_blocks = next_blocks block;
+                                    prev_blocks = prev_blocks block }) block_map;;
 
-module StringMap = Map.Make(String);;
+(*
 
-(* 
+  This is actually the abstract interpreter core. It needs the
+  following parameters:
 
-   This is actually the abstract interpreter core. It needs the
-   following parameters:
+  * `blocks: block list` the list of blocks to run on;
 
-   * `blocks: block list` the list of blocks to run on;
+  * `state_converges: state -> state -> bool` the function that
+  decides if fixed point is reached; the parameters are the old
+  state, the new state; it returns true if the states converge, false
+  otherwise;
 
-   * `state_converges: state -> state -> bool` the function that
-   decides if fixed point is reached; the parameters are the old
-   state, the new state; it returns true if the states converge, false
-   otherwise;
+  * `state_combine: state list -> state` the function used to decide
+  the state at the start of a block based on the state of previous
+  blocks.
 
-   * `state_combine: state list -> state` the function used to decide
-   the state at the start of a block based on the state of previous
-   blocks.
+  * `state_update: state -> instruction -> state` the function ran on
+  every instruction to update the state in the current block.
 
-   * `state_update: state -> instruction -> state` the function ran on
-   every instruction to update the state in the current block.
+  At the end, the final state is the combined state (via
+  `state_combine`) of all final blocks (i.e. blocks without follow up
+  blocks).
 
-   At the end, the final state is the combined state (via
-   `state_combine`) of all final blocks (i.e. blocks without follow up
-   blocks).
+  The abstract interpreter core will run these functions on a set of
+  blocks.
 
-   The abstract interpreter core will run these functions on a set of
-   blocks.
-
-   An abstract interpreter pass is made up of a set of these
-   functions.
+  An abstract interpreter pass is made up of a set of these
+  functions.
 
 *)
-(* let block_sort blocks = *)
-(*   let first_blocks = List.filter (fun block -> block.prev_blocks = []) blocks in *)
-(*   let touched, touched_count = *)
-(*     List.fold_left *)
-(*       (fun (touched_map, touched_count) block -> *)
-(*          let bool_value = List.mem block first_blocks in *)
-(*            (StringMap.add block.name bool_value touched_map, *)
-(*             if bool_value then touched_count + 1 else touched_count)) *)
-(*       (StringMap.empty, 0) *)
-(*   in *)
-(*   let block_count = List.count blocks in *)
-(*   let add_end elm list = (elm :: (List.rev list) |> List.rev in *)
-(*   let bs_iter queue touched touched_count = *)
-(*     match queue with *)
-(*       | [] -> *)
-(*   in *)
-(*     bs_iter blocks first_blocks touched touched_count [];; *)
+type 'state absint_pass = { state_combine: 'state option list -> 'state option;
+                            state_converges: 'state option -> 'state option -> bool;
+                            state_update: 'state option -> ir -> 'state option;
+                          };;
+
+type 'state block_state = int * 'state option;;
+
+let block_sort blocks pass max_iterations_per_block =
+  let blocks_state =
+    List.fold_left
+      (fun map block -> StringMap.add block.name (0, None) map)
+      StringMap.empty
+      blocks
+  in
+  let blocks_map, _ = make_block_maps blocks in
+  let first_blocks = List.filter (fun block -> block.prev_blocks = []) blocks in
+  let final_blocks = List.filter (fun block -> block.next_blocks = []) blocks in
+  let add_end elms list = (elms @ (List.rev list)) |> List.rev in
+  let extract_state (count, state) = state in
+  let rec bs_iter queue blocks_state =
+    match queue with
+      | [] ->
+          let final_blocks_states =
+            List.map
+              (fun block -> StringMap.find block.name blocks_state |> extract_state)
+              final_blocks
+          in
+            pass.state_combine final_blocks_states
+      | hd :: tl ->
+          let count, old_state = StringMap.find hd.name blocks_state in
+          let new_state = List.fold_left pass.state_update old_state hd.instructions in
+          let process_children =
+            count < max_iterations_per_block
+            && not(pass.state_converges old_state new_state)
+          in
+          let new_blocks_state =
+            StringMap.add hd.name (count + 1, new_state) blocks_state
+          in
+            if process_children
+            then
+              let children_blocks =
+                List.map (fun name -> StringMap.find name blocks_map) hd.next_blocks
+              in
+              let new_queue = add_end children_blocks tl in
+                bs_iter new_queue new_blocks_state
+            else
+              bs_iter tl new_blocks_state
+  in
+    bs_iter first_blocks blocks_state;;
+
+(* Undefined variable pass. *)
+type var_value =
+  | Uninitialized
+  | Unknown
+
+type env = var_value StringMap.t list;;
+
+module StringSet = Set.Make(String);;
+
+type state = env * string StringMap.t;;
+
+let combine_vals expr1 expr2 =
+  match (expr1, expr2) with
+    | Unknown, Unknown -> Unknown
+    | _ -> Uninitialized;;
+
+let rec eval expr env =
+  let rec env_get_var env var =
+    match env with
+      | var_map :: other_maps ->
+          if StringMap.mem var var_map
+          then Some(StringMap.find var var_map)
+          else env_get_var other_maps var
+      | [] ->
+          None
+  in
+  match expr with
+    | Number _ -> Unknown
+    | Add(expr1, expr2)
+    | Subtract(expr1, expr2)
+    | Mod(expr1, expr2)
+    | Multiply(expr1, expr2)
+    | Divide(expr1, expr2)
+    | LessThan(expr1, expr2)
+    | GreaterThan(expr1, expr2)
+    | Equal(expr1, expr2)  ->
+        combine_vals (eval expr1 env) (eval expr2 env)
+    | Not(expr) ->
+        eval expr env
+    | Var var ->
+        match env_get_var env var with
+          | None -> Uninitialized
+          | Some _ -> Unknown;;
 
 let process ir_list =
   let blocks = ir_list |> process_goto_if |> split_into_blocks in
   let block_map, label_map = make_block_maps blocks in
   let block_map = add_prev_next block_map label_map in
-  let block_list = BlockMap.fold (fun _ block list -> block :: list) block_map []
-             |> List.rev
+  let block_list = StringMap.fold (fun _ block list -> block :: list) block_map []
+                          |> List.rev
   in
     block_list;;
